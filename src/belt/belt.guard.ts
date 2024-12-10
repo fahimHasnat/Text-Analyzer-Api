88da40ad669d68a11b9caae148643a8dfc1c78f4
Fahim Hasnat <37208911+fahimHasnat@users.noreply.github.com>
2024-12-10 21:44:09 +0600

    fix: log removal

--- src/belt/belt.guard.ts
+++ src/belt/belt.guard.ts
@@ -1,13 +1,12 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class BeltGuard implements CanActivate {
   canActivate(
-    context: ExecutionContext,
+    context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
-    console.log(request);
     return true;
   }
 }
